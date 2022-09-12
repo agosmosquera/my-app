d747e69463edcc6ddd564f32ec657b4c25b3f95d
agos.mosquera <agosmosquera23@gmail.com>
2022-09-12 10:57:50 -0300

    itemDetail

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -9,7 +9,7 @@ export const ItemDetailContainer = () => {
     const {id} = useParams()
     const [isLoading, setIsLoading] = useState([true]);
     const getProduct =() => new Promise((resolve, reject) => {
-      setTimeout(() => resolve(producto.find(producto =>producto.id ===Number(id))), 200)
+      setTimeout(() => resolve(producto.find(producto =>producto.id ===Number(id))), 2000)
     })
     useEffect(() => {
         fetch("../json/productos.json")
@@ -18,7 +18,7 @@ export const ItemDetailContainer = () => {
         .then (response => {setTimeout(()=>{
           setproductoCard(response)
           setIsLoading(false);
-      }, 200)})
+      }, 2000)})
         .catch(error => {
             console.error(error)})
     }, [id])
