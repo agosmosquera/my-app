05b6fd88144ccc8101aaa9883620015f1266c7cf
agos.mosquera <agosmosquera23@gmail.com>
2022-09-13 20:20:19 -0300

    MuestraError

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -3,22 +3,22 @@ import  {ItemDetalle}  from '../ItemDetalle/ItemDetalle'
 import {Counter} from '../ItemCount/Counter'
 import swal from 'sweetalert';
 
-export const ItemDetail = (producto) => {
+export const ItemDetail = ({item}) => {
   function onAdd(count){
       swal( '¡Agregado al carrito!', 'Se han seleccionado ' + count + ' productos', 'success' )
     }
 return (
   <div>
-      <div className="card bg-light mb-3" key={producto.id} style={{maxWidth: '15rem', margin: '5px'}}>
-                               <div className="card-header" >{producto.nombre}</div>
+      <div className="card bg-light mb-3" key={item.id} style={{maxWidth: '15rem', margin: '5px'}}>
+                               <div className="card-header" >{item.nombre}</div>
                                    <div className="card-body">
-                                   <img src={producto.img} alt={producto.nombre} width="100%" height="200"/>
-                                   <p className="card-text">Marca: {producto.marca}</p>
-                                   <p className="card-text">Precio: ${producto.precio}</p>
-                                   <p className="card-text">Descripcion: {producto.descripcion}</p>
+                                   <img src={item.img} alt={item.nombre} width="100%" height="200"/>
+                                   <p className="card-text">Marca: {item.marca}</p>
+                                   <p className="card-text">Precio: ${item.precio}</p>
+                                   <p className="card-text">Descripcion: {item.descripcion}</p>
                                 </div>
                                 <div>
-                                   <Counter stock={producto.stock} producto={producto} onAdd={onAdd}/>
+                                   <Counter stock={item.stock} producto={item} onAdd={onAdd}/>
                                </div>
                            </div>
   </div>
